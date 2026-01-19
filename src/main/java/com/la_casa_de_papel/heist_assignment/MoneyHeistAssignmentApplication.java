e7fd5cbed3e3b0b37b807eeb280379b52b72ba43
irastego <ivan.rastegorac@gmail.com>
2026-01-19 11:15:36 +0100

    Initial commit: Project setup with dependencies

---
+++ src/main/java/com/la_casa_de_papel/heist_assignment/MoneyHeistAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.la_casa_de_papel.heist_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHeistAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHeistAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/la_casa_de_papel/heist_assignment/MoneyHeistAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la_casa_de_papel.heist_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHeistAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
