14e48fa7b29b93eb88d3459080cc6afa28bee0c8
irastego <ivan.rastegorac@gmail.com>
2026-01-21 13:25:34 +0100

    SBSS-02 Implement Update Skills endpoint

--- src/main/java/com/la_casa_de_papel/heist_assignment/controller/MemberController.java
+++ src/main/java/com/la_casa_de_papel/heist_assignment/controller/MemberController.java
@@ -1,13 +1,12 @@
 package com.la_casa_de_papel.heist_assignment.controller;
 
 import com.la_casa_de_papel.heist_assignment.dto.MemberRequest;
+import com.la_casa_de_papel.heist_assignment.dto.UpdateSkillsRequest;
 import com.la_casa_de_papel.heist_assignment.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -30,5 +29,23 @@ public class MemberController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/member/{id}/skills")
+    public ResponseEntity<Void> updateSkills(
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateSkillsRequest request) {
+
+        memberService.updateSkills(id, request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .build()
+                .toUri();
+
+        return ResponseEntity
+                .noContent()
+                .header("Content-Location", location.toString())
+                .build();
     }
 }
